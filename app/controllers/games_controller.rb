752138f322e71e8f297cf49265a88e5939720f60
Michael Pope <popemray1986@gmail.com>
2018-11-18 18:44:26 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
-  before_action :set_game, only [:show, :update]
+  before_action :set_game, only: [:show, :update]
 
   def Create
     game = Game.Create(game_params)
