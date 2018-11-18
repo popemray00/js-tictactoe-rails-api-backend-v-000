82c8996cb233300ea8cced39e30abff7ce9bed6a
Michael Pope <popemray1986@gmail.com>
2018-11-18 18:54:29 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-resources :games, only: [:index, :show, :create, :update]
+  resources :games, only: [:index, :show, :create, :update]
 
 end
