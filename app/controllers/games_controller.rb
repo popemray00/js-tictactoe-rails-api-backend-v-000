b7ccb16ec7913efe9cee815607979e27efd98794
Michael Pope <popemray1986@gmail.com>
2018-11-18 18:58:00 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,7 +12,7 @@ class GamesController < ApplicationController
   end
 
   def create
-    game = Game.Create(game_params)
+    game = Game.create(game_params)
     render json: game, status: 201
   end
 
