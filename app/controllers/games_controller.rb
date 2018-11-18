667b11b5f108cd6b7c4cd347ac8061a49ea917e5
Michael Pope <popemray1986@gmail.com>
2018-11-18 18:43:58 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,7 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   before_action :set_game, only [:show, :update]
+
   def Create
     game = Game.Create(game_params)
     render json: game, status: 201
